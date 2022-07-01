217471bdb90cec4e05fd40d49eb3957666b46e14
pedroperegrinaa <57403682+pedroperegrinaa@users.noreply.github.com>
2022-07-01 04:02:18 -0300

    :adhesive_bandage: delete not necessary console.log

--- src/index.ts
+++ src/index.ts
@@ -4,7 +4,6 @@ import showDistance from './showDistance'
 
 yargs(hideBin(process.argv))
   .command('distance <distance>', 'Calculate distance MGLT and show all starships', () => {}, (argv) => {
-    console.info(argv)
     showDistance(argv.distance)
   })
   .demandCommand(1)
